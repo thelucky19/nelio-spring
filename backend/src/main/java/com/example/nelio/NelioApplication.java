cf16e2828f200fdbf101b0e4e9d4a2f40f4a54f6
Joao Lucas Fernandes dos Santos <joao.lucas@invillia.com>
2022-01-14 19:27:12 -0300

    Project created

---
+++ backend/src/main/java/com/example/nelio/NelioApplication.java
@@ -0,0 +1,13 @@
+package com.example.nelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/nelio/NelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
