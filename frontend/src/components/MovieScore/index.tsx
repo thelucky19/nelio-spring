f70a7c99e93f8ecf8ed8c72069af93a971461352
Joao Lucas Fernandes dos Santos <joao.lucas@invillia.com>
2022-01-16 23:52:45 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,20 +1,17 @@
 import MovieStars from 'components/MovieStars';
 import './styles.css';
 
-const movie = {
-    id: 1,
-    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5
-};
+type Props = {
+    score: number;
+    count: number;
+}
 
-function MovieScore() {
+function MovieScore({ score, count } : Props) {
     return (
         <div className="dsmovie-score-container">
-        <p className="dsmovie-score-value">{movie.score > 0 ? movie.score.toFixed(1) : '-'}</p>
-        <MovieStars />
-        <p className="dsmovie-score-count">{movie.count} avaliações</p>
+        <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+        <MovieStars score={score}/>
+        <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
     );
 }
